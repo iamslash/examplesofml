dfaea63188a6fbe21ac2cc06c1edd666eeebe041
iamslash <iamslash@gmail.com>
2017-04-10 19:22:56 +0900

    basic ing...

--- tensorflow/basic.py
+++ tensorflow/basic.py
@@ -1,1 +1,13 @@
- 
+
+def test():
+    import tensorflow as tf
+    tf.logging.set_verbosity(tf.logging.ERROR)
+
+    hello = tf.constant("Hello World")
+    sess = tf.Session()
+    print(sess.run(hello))
+
+
+   
+if __name__ == "__main__":
+    test()
