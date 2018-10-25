a192274d4cc1c69b93739acea4c773c31bedcdc0
iamslash <iamslash@gmail.com>
2018-10-25 16:38:58 +0900

    finally tested using CONV

--- mnistdcgan/mnistdcgan.py
+++ mnistdcgan/mnistdcgan.py
@@ -8,7 +8,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Reshape
 from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D
-from keras.layers import LeakyReLU, Dropout
+from keras.layers import LeakyReLU, Dropout, ReLU
 from keras.layers import BatchNormalization
 from keras.optimizers import Adam, RMSprop
 
@@ -34,7 +34,7 @@ class DcGan(object):
         self.img_row = row
         self.img_col = col
         self.img_chn = chn
-        self.latent_size = 64
+        self.latent_size = 100
         self.hidden_size = 256
         self.img_size = row * col * chn
 
@@ -45,45 +45,129 @@ class DcGan(object):
 
     def build_discriminator(self):
         seq = Sequential()
-        seq.add(Dense(self.hidden_size, input_dim=self.img_size))
+        depth = self.latent_size
+        dropout = 0.4
+        # In: 28 x 28 x 1, depth = 1
+        # Out: 14 x 14 x 1, depth=64
+        input_shape = (self.img_row, self.img_col, self.img_chn)
+        seq.add(Conv2D(depth*1, 5, strides=2, input_shape=input_shape, padding='same'))
         seq.add(LeakyReLU(alpha=0.2))
-        seq.add(Dense(self.hidden_size))
+        seq.add(Dropout(dropout))
+
+        seq.add(Conv2D(depth*2, 5, strides=2, padding='same'))
         seq.add(LeakyReLU(alpha=0.2))
+        seq.add(Dropout(dropout))
+
+        seq.add(Conv2D(depth*4, 5, strides=2, padding='same'))
+        seq.add(LeakyReLU(alpha=0.2))
+        seq.add(Dropout(dropout))
+
+        seq.add(Conv2D(depth*8, 5, strides=1, padding='same'))
+        seq.add(LeakyReLU(alpha=0.2))
+        seq.add(Dropout(dropout))
+
+        # Out: 1-dim probability
+        seq.add(Flatten())
         seq.add(Dense(1))
-        seq.add(Activation('sigmoid'))    
-        seq.summary()    
+        seq.add(Activation('sigmoid'))
+        seq.summary()
         return seq
 
     def build_generator(self):
         seq = Sequential()
-        seq.add(Dense(self.hidden_size, input_dim=self.latent_size))
-        seq.add(LeakyReLU())
-        seq.add(Dense(self.hidden_size))
-        seq.add(LeakyReLU())
-        seq.add(Dense(self.img_size))
-        seq.add(Activation('tanh')) 
-        seq.summary()       
+        dropout = 0.4
+        depth = self.latent_size * 4
+        dim = 7
+        # In: 100
+        # Out: dim x dim x depth
+        seq.add(Dense(dim*dim*depth, input_dim=self.latent_size))
+        seq.add(BatchNormalization(momentum=0.9))
+        seq.add(Activation('relu'))
+        seq.add(Reshape((dim, dim, depth)))
+        seq.add(Dropout(dropout))
+
+        # In: dim x dim x depth
+        # Out: 2*dim x 2*dim x depth/2
+        seq.add(UpSampling2D())
+        seq.add(Conv2DTranspose(int(depth/2), 5, padding='same'))
+        seq.add(BatchNormalization(momentum=0.9))
+        seq.add(Activation('relu'))
+
+        seq.add(UpSampling2D())
+        seq.add(Conv2DTranspose(int(depth/4), 5, padding='same'))
+        seq.add(BatchNormalization(momentum=0.9))
+        seq.add(Activation('relu'))
+
+        seq.add(Conv2DTranspose(int(depth/8), 5, padding='same'))
+        seq.add(BatchNormalization(momentum=0.9))
+        seq.add(Activation('relu'))
+
+        # Out: 28 x 28 x 1 grayscale image [0.0,1.0] per pix
+        seq.add(Conv2DTranspose(1, 5, padding='same'))
+        seq.add(Activation('sigmoid'))
+        seq.summary()
         return seq
-    
+
     def build_discriminator_model(self):
+        optimizer = RMSprop(lr=0.0002, decay=6e-8)
         seq = Sequential()
         seq.add(self.D)
-        seq.compile(optimizer=Adam(lr=0.0002), loss='binary_crossentropy', metrics=['accuracy'])
+        seq.compile(loss='binary_crossentropy', optimizer=optimizer,
+            metrics=['accuracy'])
         return seq
 
     def build_adversarial_model(self):
+        optimizer = RMSprop(lr=0.0001, decay=3e-8)
         seq = Sequential()
         seq.add(self.G)
         seq.add(self.D)
-        seq.compile(optimizer=Adam(lr=0.0002), loss='binary_crossentropy',metrics=['accuracy'])
-        return seq
+        seq.compile(loss='binary_crossentropy', optimizer=optimizer,
+            metrics=['accuracy'])
+        return seq      
+
+    # def build_discriminator(self):
+    #     seq = Sequential()
+    #     seq.add(Dense(self.hidden_size, input_dim=self.img_size))
+    #     seq.add(LeakyReLU(alpha=0.2))
+    #     seq.add(Dense(self.hidden_size))
+    #     seq.add(LeakyReLU(alpha=0.2))
+    #     seq.add(Dense(1))
+    #     seq.add(Activation('sigmoid'))    
+    #     seq.summary()    
+    #     return seq
+
+    # def build_generator(self):
+    #     seq = Sequential()
+    #     seq.add(Dense(self.hidden_size, input_dim=self.latent_size))
+    #     seq.add(ReLU())
+    #     seq.add(Dense(self.hidden_size))
+    #     seq.add(ReLU())
+    #     seq.add(Dense(self.img_size))
+    #     seq.add(Activation('tanh')) 
+    #     seq.summary()
+    #     return seq
+    
+    # def build_discriminator_model(self):
+    #     seq = Sequential()
+    #     seq.add(self.D)
+    #     seq.compile(optimizer=Adam(lr=0.0002), loss='binary_crossentropy', metrics=['accuracy'])
+    #     return seq
+
+    # def build_adversarial_model(self):
+    #     seq = Sequential()
+    #     seq.add(self.G)
+    #     seq.add(self.D)
+    #     seq.compile(optimizer=Adam(lr=0.0002), loss='binary_crossentropy',metrics=['accuracy'])
+    #     return seq
 
 class MnistDcGan(object):
     def __init__(self):
         self.dcgan = DcGan()
 
         self.x_train = input_data.read_data_sets("mnist", one_hot=True).train.images
-        self.x_train = self.x_train.reshape(-1, self.dcgan.img_size).astype(np.float32)
+        #self.x_train = self.x_train.reshape(-1, self.dcgan.img_size).astype(np.float32)
+        self.x_train = self.x_train.reshape(-1, 
+            self.dcgan.img_row, self.dcgan.img_col, self.dcgan.img_chn).astype(np.float32)
 
     def train(self, epoch_size=200, batch_size=128, save_interval=1):
         real_labels = np.ones([batch_size, 1])
@@ -91,9 +175,12 @@ class MnistDcGan(object):
         comp_labels = np.concatenate((real_labels, fake_labels))
 
         for i in range(epoch_size):
-            real_imgs = self.x_train[np.random.randint(0, self.x_train.shape[0], size=batch_size), :]
-            
-            fake_imgs = np.random.uniform(-1, 1, size=[batch_size, self.dcgan.latent_size])
+            #real_imgs = self.x_train[np.random.randint(0, self.x_train.shape[0], size=batch_size), :]
+
+            real_imgs = self.x_train[np.random.randint(0,
+                self.x_train.shape[0], size=batch_size), :, :, :]
+
+            fake_imgs = np.random.uniform(-1.0, 1.0, size=[batch_size, self.dcgan.latent_size])
             fake_imgs = self.dcgan.G.predict(fake_imgs)
             
             comp_imgs = np.concatenate((real_imgs, fake_imgs))
@@ -106,11 +193,11 @@ class MnistDcGan(object):
             # ============================================================ #
             #                    Train the generator                       #
             # ============================================================ #
-            fake_imgs = np.random.uniform(-1, 1, size=[batch_size, self.dcgan.latent_size])
+            fake_imgs = np.random.uniform(-1.0, 1.0, size=[batch_size, self.dcgan.latent_size])
             a_loss = self.dcgan.AM.train_on_batch(fake_imgs, real_labels)
 
-            msg = "%d: [D loss: %f, acc: %f]" % (i, d_loss[0], d_loss[1])
-            msg = "%s  [A loss: %f, acc: %f]" % (msg, a_loss[0], a_loss[1])
+            msg = "%04d: [D loss: %f, acc: %f]" % (i, d_loss[0], d_loss[1])
+            msg =   "%s  [A loss: %f, acc: %f]" % (msg, a_loss[0], a_loss[1])
             print(msg)
             if (i+1) % save_interval == 0:
                 fake_img = np.random.uniform(-1, 1, size=[16, self.dcgan.latent_size])
@@ -134,7 +221,9 @@ class MnistDcGan(object):
             os.mkdir(d)
 
 if __name__ == '__main__':
+    import shutil
+    shutil.rmtree(IMGDIR, ignore_errors=True)
     mdg = MnistDcGan()
     timer = ElapsedTimer()
-    mdg.train(epoch_size=128, batch_size=128, save_interval=1)
-    timer.elapsed_time()    
+    mdg.train(epoch_size=1024, batch_size=128, save_interval=64)
+    timer.elapsed_time()
