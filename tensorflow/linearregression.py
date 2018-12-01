a14f5900441ff7b54f67a393b7fd3f5af857b4a7
David Sun <iamslash@gmail.com>
2018-12-01 15:41:30 +0900

    fixed type prefix

--- tensorflow/linearregression.py
+++ tensorflow/linearregression.py
@@ -2,25 +2,25 @@ import tensorflow as tf
 
 def main():
     tf.set_random_seed(111)
-    X = [1, 2, 3]
-    Y = [1, 2, 3]
-    W = tf.Variable(tf.random_normal([1]), name='W')
-    b = tf.Variable(tf.random_normal([1]), name='b')
-    H = X * W + b
-    C = tf.reduce_mean(tf.square(H - Y))
-    O = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-    T = O.minimize(C)
+    l_X = [1, 2, 3]
+    l_Y = [1, 2, 3]
+    t_W = tf.Variable(tf.random_normal([1]), name='W')
+    t_b = tf.Variable(tf.random_normal([1]), name='b')
+    t_H = l_X * t_W + t_b
+    t_C = tf.reduce_mean(tf.square(t_H - l_Y))
+    t_O = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+    t_T = t_O.minimize(t_C)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for step in range(2000):
-            sess.run(T)
+            sess.run(t_T)
             if step % 20 == 0:
-                f_cost = sess.run(C)
-                f_W = sess.run(W)
-                f_b = sess.run(b)
+                f_cost = sess.run(t_C)
+                l_W = sess.run(t_W)
+                l_b = sess.run(t_b)
 
                 #print("step = {:7d} loss = {:5.3f} W = {:5.3f} b = {:5.3f}".format(step, f_cost, f_W, f_b) )
-                print(step, f_cost, f_W, f_b)
+                print("{:7d}".format(step), "{:10.7f}".format(f_cost), l_W, l_b)
 
 if __name__ == "__main__":
     main()
